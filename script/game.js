05650bec2eb31b66b89373cca6a99a902681cadb
貓虎皮 <jhou5846@gmail.com>
2024-02-08 11:47:13 +0800

    新增逐字顯示文字之動畫系統
    1. 新增「逐字顯示文字」動畫系統
    2. 將`sceneVariable`的初始化移至`renderScene`內
    3. 新增文字動畫系統之樣式參數標籤至`data/dialog.json > story > 0 > dialog > "--@未初始化>*" > message`，演示顯示速度、文字粗細的的本變化

--- script/game.js
+++ script/game.js
@@ -1,9 +1,9 @@
 'use strict';
-async function initGameCycle(initData){
+async function initGameCycle(initData) {
 	/* declare shared object variable */
-	const {CW, CH} = initData;
-	const {cvs, ctx} = initData;
-	const {mouse} = initData;
+	const { CW, CH } = initData;
+	const { cvs, ctx } = initData;
+	const { mouse } = initData;
 
 	/* all manner of colors */
 	const color = {
@@ -123,20 +123,46 @@ async function initGameCycle(initData){
 
 	/* change background */
 	const sceneVariable = {};
-	sceneVariable['place'] = '未初始化';
-	sceneVariable['object'] = '無';
-	sceneVariable['s'] = '';
-	sceneVariable['v'] = '';
-	sceneVariable['o'] = '';
+	const tempCvs = document.createElement('canvas'),
+		tempCtx = tempCvs.getContext('2d');
+	[tempCvs.width, tempCvs.height] = [CW, CH];
+	let lastTime = Date.now();
 	async function renderScene(sceneIndex) {
-		let [sceneType, sceneId] = sceneIndex;
+		var [sceneType, sceneId] = sceneIndex;
+		var sceneIndexText = `${sceneType}-${sceneId}`;
+		let sceneChanged = false;
+		if (sceneIndexText !== sceneVariable['laseSceneIndexText']) {
+			sceneVariable['laseSceneIndexText'] = sceneIndexText;
+			sceneChanged = true;
+		}
+
+		let currentTime = Date.now();
+		let deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+
 		if (sceneType == 'menu') {
 			await renderMenu(sceneId);
 		} else if (sceneType == 'dialog') {
+			if (sceneChanged) {
+				sceneVariable['place'] = '未初始化';
+				sceneVariable['object'] = '無';
+				sceneVariable['s'] = '';
+				sceneVariable['v'] = '';
+				sceneVariable['o'] = '';
+				sceneVariable['currentDialogKey'] = `--@${sceneVariable['place']}>*`;
+			}
+
 			let dialogLevel = parseInt(sceneId);
 			let currentStoryDialog = dialogData.story[dialogLevel - 1].dialog;
 
 			let action = `${sceneVariable['s']}-${sceneVariable['v']}-${sceneVariable['o']}`;
+			let actionChanged = false;
+			if (action !== sceneVariable['lastAction']) {
+				sceneVariable['lastAction'] = action;
+				actionChanged = true;
+			}
+
+			var dialogBoxDisplay = { x: 470, y: 730, w: 1400, h: 300 };
 			let dialog = {
 				"image": false,
 				"message": "",
@@ -144,52 +170,117 @@ async function initGameCycle(initData){
 				"at": sceneVariable['place'],
 				"check": sceneVariable['object']
 			};
-			for (let dialogKey of [
-				`${action}@${sceneVariable['place']}>${sceneVariable['object']}`,
-				`${action}@${sceneVariable['place']}>*`,
-				`${action}@*>*`
-			]) {
-				if (dialogKey in currentStoryDialog) {
-					dialog = currentStoryDialog[dialogKey];
-					if (dialog.at !== false) sceneVariable['place'] = dialog.at;
-					if (dialog.check !== false) sceneVariable['object'] = dialog.check;
-					break;
+			if ((actionChanged && ![sceneVariable['s'], sceneVariable['v'], sceneVariable['o']].includes('')) || sceneChanged) {
+				sceneVariable['currentDialogKey'] = '--@*>*';
+				for (let dialogKey of [
+					`${action}@${sceneVariable['place']}>${sceneVariable['object']}`,
+					`${action}@${sceneVariable['place']}>*`,
+					`${action}@*>*`
+				]) {
+					if (dialogKey in currentStoryDialog) {
+						dialog = currentStoryDialog[dialogKey];
+						sceneVariable['currentDialogKey'] = dialogKey;
+						if (dialog.at !== false) sceneVariable['place'] = dialog.at;
+						if (dialog.check !== false) sceneVariable['object'] = dialog.check;
+						break;
+					}
 				}
+
+				var fontSize = 50;
+				var dialogBoxPadding = 50;
+				var lineSpacing = 0;
+				var lineLength = Math.floor((dialogBoxDisplay.w - dialogBoxPadding * 2) / fontSize);
+				var lineNumber = Math.ceil(dialog.message.length / lineLength);
+				var maxLineNumber = Math.floor((dialogBoxDisplay.h - dialogBoxPadding * 2) / (fontSize + lineSpacing));
+
+				let attribute = [];
+				let messageData = [];
+				let charList = dialog.message.split('');
+
+				while (charList.length > 0) {
+					let char = charList.shift();
+					let line = Math.floor(messageData.length / lineLength);
+					let index = messageData.length % lineLength;
+					let newChar = undefined;
+					switch (char) {
+						case '\\':
+							if (['\\', '{', '}'].includes(charList[0])) {
+								newChar = charList.shift();
+							} else {
+								attribute.push(charList.splice(0, charList.indexOf('{')).join('').split('_'));
+								charList.shift();
+							}
+							break;
+						case '{':
+							// error
+							break;
+						case '}':
+							attribute.pop();
+							break;
+						default:
+							newChar = char;
+							break;
+					}
+					if (newChar !== undefined) {
+						messageData.push({
+							x: dialogBoxDisplay.x + dialogBoxPadding + index * fontSize,
+							y: dialogBoxDisplay.y + dialogBoxPadding + line * (fontSize + lineSpacing),
+							char: newChar,
+							attribute: { miniSec: (100).toString(), size: fontSize.toString(), ...Object.fromEntries(attribute) } // 透過轉換為 object 來去除掉重複的鍵，且讓其保留最後設定的值 
+						});
+					}
+				}
+				sceneVariable['dialogMessageData'] = messageData;
+				sceneVariable['dialogMessageAniChar'] = 0;
+				sceneVariable['dialogMessageAniTimeLeft'] = 0;
+				sceneVariable['dialogMessageAniStartTime'] = currentTime;
+				[tempCvs.width, tempCvs.height] = [CW, CH]; // 將 tempCvs 重置為透明畫布
+			}
+			if (sceneVariable['currentDialogKey'] in currentStoryDialog) {
+				dialog = currentStoryDialog[sceneVariable['currentDialogKey']];
 			}
 			if (dialog.image) {
 				ctx.drawImage(await getImage(dialog.image), 0, 0, CW, CH); // scene background
 			}
 			// words box
 			ctx.lineWidth = 5;
-			var fontSize = 50;
-			ctx.font = `${fontSize}px 微軟正黑`;
-			ctx.textAlign = 'left';
-			ctx.textBaseline = 'top';
 			ctx.fillStyle = '#00000088';
 			ctx.strokeStyle = color.buttonHover;
-			var dialogBoxDisplay = { x: 470, y: 730, w: 1400, h: 300 };
 			var { x, y, w, h } = dialogBoxDisplay;
 			ctx.fillRect(x, y, w, h);
 			ctx.strokeRect(x, y, w, h);
-			var [ x, y, w, h ] = [0, 0, 0, 0]
+			var [x, y, w, h] = [0, 0, 0, 0]
 
 			// dialog message
-			var dialogBoxPadding = 50;
-			var lineSpacing = 0;
-			var lineLength = Math.floor((dialogBoxDisplay.w - dialogBoxPadding * 2) / fontSize);
-			var lineNumber = Math.ceil(dialog.message.length / lineLength);
-			var maxLineNumber = Math.floor((dialogBoxDisplay.h - dialogBoxPadding * 2) / (fontSize + lineSpacing));
-			if (dialog?.color != undefined) {
-				ctx.fillStyle = dialog?.color;
-				ctx.strokeStyle = dialog?.color;
-			} else {
-				ctx.fillStyle = 'white';
-				ctx.strokeStyle = 'white';
+			if (sceneVariable['dialogMessageAniChar'] < sceneVariable['dialogMessageData'].length) {
+				tempCtx.textAlign = 'left';
+				tempCtx.textBaseline = 'top';
+				let aniDeltaTime = currentTime - sceneVariable['dialogMessageAniStartTime'] - sceneVariable['dialogMessageAniTimeLeft'];
+				let charData = sceneVariable['dialogMessageData'][sceneVariable['dialogMessageAniChar']];
+				let charMiniSec = parseInt(charData.attribute.miniSec);
+				while (aniDeltaTime >= charMiniSec) {
+					if ('italic' in charData.attribute || 'oblique' in charData.attribute) charData.attribute.style = 'oblique';
+					if ('bold' in charData.attribute || 'bolder' in charData.attribute) charData.attribute.weight = 'bolder';
+					if ('light' in charData.attribute || 'lighter' in charData.attribute) charData.attribute.weight = 'lighter';
+					tempCtx.font = [
+						charData.attribute.style ? charData.attribute.style : 'normal',
+						charData.attribute.variant ? charData.attribute.variant : 'normal',
+						charData.attribute.weight ? charData.attribute.weight : 'normal',
+						`${charData.attribute.size ? parseFloat(charData.attribute.size) : 5}px`,
+						charData.attribute.family ? charData.attribute.family : '微軟正黑'
+					].join(' ');
+					tempCtx.fillStyle = charData.attribute.color ? charData.attribute.color : 'white';
+					tempCtx.fillText(charData.char, charData.x, charData.y);
+
+					aniDeltaTime -= charMiniSec;
+					sceneVariable['dialogMessageAniTimeLeft'] += charMiniSec;
+					sceneVariable['dialogMessageAniChar']++;
+					charData = sceneVariable['dialogMessageData'][sceneVariable['dialogMessageAniChar']];
+					if (charData === undefined) break;
+					charMiniSec = parseInt(charData.attribute.miniSec);
+				}
 			}
-			for (let i = 0; i < lineNumber; i += 1) {
-				let lineContent = dialog.message.split('').splice(i * lineLength, lineLength).join('');
-				ctx.fillText(lineContent, dialogBoxDisplay.x + dialogBoxPadding, dialogBoxDisplay.y + dialogBoxPadding + i * (fontSize + lineSpacing));
-			}
+			ctx.drawImage(tempCvs, 0, 0);
 			if (lineNumber > maxLineNumber) {
 				throw Error(`'dialog.message' overflow! Message length is ${dialog.message.length}, but it only allow ${lineLength} * ${maxLineNumber}`);
 			}
