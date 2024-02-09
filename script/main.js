df5c8da64e0536b06d5a86b62a9bce52f3198771
貓虎皮 <jhou5846@gmail.com>
2024-02-09 14:38:00 +0800

    新增文字效果、新字卡動畫、字卡區捲動、字卡置末、截圖系統、調快畫面更新頻率
    1. `dialog`文字效果參數新增`deltaX`、`deltaY`
    2. 未使用過的字卡繪製「發光效果」
    3. 新得到的字卡加上「飛入動畫」
    4. `dialog`的`glow`支援顏色以及`color.wordBoxSAndO`、`color.wordBoxV`顏色的直接套用
    5. 將畫面更新間隔改為30毫秒
    6. 字卡區域捲動功能
    7. 將「從`s`、`v`、`o`欄位取出的字卡」至於字卡清單的末端
    8. 將沒有詞性資料的字卡繪製為與禁用之按鈕相同的顏色
    9. 新增截圖功能及動畫

--- script/main.js
+++ script/main.js
@@ -7,10 +7,17 @@
 	cvs.style.setProperty('--w-h-ratio', `${CW}/${CH}`);
 
 	/* mouse event */
-	const mouse = { x: 0, y: 0, click: false, up: false, down: false, over: false, leave: false };
+	const mouse = {
+		x: 0, y: 0,
+		click: false,
+		up: false, down: false,
+		over: false, leave: false, 
+		deltaX: 0, deltaY: 0, deltaZ: 0, deltaZoom: 0, 
+		screenshot: false
+	};
 	var gameCycle = await initGameCycle({ cvs, ctx, mouse, CW, CH });
 	let boundingRect = cvs.getBoundingClientRect();
-	function updateMousePosition(event){
+	function updateMousePosition(event) {
 		[mouse.x, mouse.y] = [(event.pageX - boundingRect.x) / boundingRect.width * CW, (event.pageY - boundingRect.y) / boundingRect.height * CH];
 	}
 	cvs.addEventListener('mousemove', event => {
@@ -27,6 +34,29 @@
 	cvs.addEventListener('mouseup', event => {
 		updateMousePosition(event);
 		mouse.up = true;
+	});
+	cvs.addEventListener('wheel', event => {
+		updateMousePosition(event);
+		if (mouse.wheelPreventDefault && mouse.wheelPreventDefault()) {
+			event.preventDefault();
+		}
+		if (event.ctrlKey) {
+			mouse.deltaZoom += event.deltaY;
+		} else {
+			mouse.deltaX += event.deltaX;
+			mouse.deltaY += event.deltaY;
+			mouse.deltaZ += event.deltaZ;
+		}
+	});
+	cvs.addEventListener('contectmenu', event => {
+		updateMousePosition(event);
+		event.preventDefault();
+	});
+	window.addEventListener('keydown', event => {
+		if(!(event.ctrlKey || event.altKey || event.shiftKey) && event.key == 'F8'){
+			event.preventDefault();
+			mouse.screenshot = true;
+		}
 	});
 	window.addEventListener("resize", () => {
 		boundingRect = cvs.getBoundingClientRect();
